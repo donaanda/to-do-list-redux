337498465e58134be49f3b2db7436f2a5b3c3fee
Dona Anda <ceka_dona@yahoo.com>
2018-05-14 18:00:52 -0700

    partially completed

--- src/components/list.js
+++ src/components/list.js
@@ -13,7 +13,11 @@ class List extends Component {
         const {listData} = this.props;
 
         const listItems = listData.map((item, index) => {
-            return <li className="collection-item" key={index}>{item.title}</li>
+            return (
+                <li className="collection-item" key={index}>
+                    <Link to={`/item/${item._id}`}>{item.title}</Link>
+                </li>
+            )
         });
 
         return (
