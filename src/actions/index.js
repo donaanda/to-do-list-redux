0fd22f13bf7386df0a5acb6555c5496d9b8c9580
Dona Anda <ceka_dona@yahoo.com>
2018-05-14 17:30:25 -0700

    completed add-items

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,14 +2,23 @@ import types from './types';
 import axios from 'axios';
 
 const BASE_URL = 'http://api.reactprototypes.com';
-const API_KEY = '?key=c318demouser';
-//const API_KEY = '?key=csgo8899';
+//const API_KEY = '?key=c318demouser';
+const API_KEY = '?key=csgo8899';
 
 export function getListData() {
     const response = axios.get(`${BASE_URL}/todos${API_KEY}`);
 
     return {
         type: types.GET_LIST_DATA,
+        payload: response
+    }
+}
+
+export function addToDoItem(item) {
+    const response = axios.post(`${BASE_URL}/todos/${API_KEY}`, item);
+
+    return {
+        type: types.ADD_ITEM,
         payload: response
     }
 }
